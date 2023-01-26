d7f6b4a0dab9c6eb7dd72eecb76074af73493d3f
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-01-26 20:19:35 -0300

    req02 concluido

--- src/models/User.js
+++ src/models/User.js
@@ -1,17 +1,23 @@
 
 
 const UsersSchema = (sequelize, DataTypes) => {
-    const UserTable = sequelize.define('user',{
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING,
-        image: DataTypes.STRING,
-    }, {
-        tableName: 'users',
-        underscored: true,
-        timestamps:false
-    })
-
+    const UserTable = sequelize.define('User',{
+        id:{
+            type: DataTypes.INTERGE,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement:true,
+        },
+            displayName: DataTypes.STRING,
+            email: DataTypes.STRING,
+            password: DataTypes.STRING,
+            image: DataTypes.STRING,
+        }, {
+            tableName: 'users',
+            underscored: true,
+            timestamps:false
+        })
+        
     UserTable.associate = (models) => {
         UserTable.belongsTo(models.User, {
             as: 'user',
